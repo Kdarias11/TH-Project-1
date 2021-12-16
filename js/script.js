f038e04d25683559a23ee097aedbcae98237efd7
Kdarias11 <92054408+Kdarias11@users.noreply.github.com>
2021-12-16 14:35:17 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,55 +14,55 @@ project 1 - A Random Quote Generator
 var quotes = [
   {  
     quote:'Live as if you were to die tomorrow. Learn as if you were to live forever',
-    source:'-Mahatma Gandi',
+    source:'Mahatma Gandi',
     citation:'',
     year:''
 
   },
   {
     quote:'That which does no kill us makes us stronger.',
-    source:'-Friedrich Nietzche',
+    source:'Friedrich Nietzche',
     citation:'Twightlight of the Idols',
     year:'1888'
   },  
   {
     quote:'Do what you can, with what you have, where you are.',
-    source:'-Theodore Roosevelt',
+    source:'Theodore Roosevelt',
     citation:'Speach at Havard',
     year:'1905'
 
   },
   {
     quote:'If opportunity does not knock, build a door. ',
-    source:'-Milton Berle',
+    source:'Milton Berle',
     citation:'One Door Closes, Another Door Opens',
     year:'1995'
 
   },
   {
     quote:'Strive not to be success, but rather to be of value',
-    source:'-Albert Einstein',
+    source:'Albert Einstein',
     citation:'LIFE Magazine',
     year:'1955'
 
   },
   {
     quote:'Do not let what you cannot do interfere with what you can do.',
-    source:'-John Wooden',
+    source:'John Wooden',
     citation:'',
     year:'',
 
   },
   {
     quote:'I havent failed. I have just found 10,000 ways that it wont work.',
-    source:'-Thomas Edison',
+    source:'Thomas Edison',
     citation:'His Life and Iventions',
     year:'1910'
 
   },
   {
     quote:'Believe and act as if it were impossible to fail.',
-    source:'-Charles Kettering',
+    source:'Charles Kettering',
     citation:'',
     year:''
 
@@ -126,7 +126,7 @@ function printQuote() {
   // appropriate className, and year property to the HTML 
   //string
 if (currentQuote.year)  {
-  sourceParagraph += "<span class='year'>" + currentQuote.date + "</span>";
+  sourceParagraph += "<span class='year'>" + currentQuote.year + "</span>";
 
 }
 
@@ -135,9 +135,9 @@ if (currentQuote.year)  {
   sourceParagraph += "</p>"
   // 6. set the innerHTML of the quote-box div to equal the 
   // complete HTML string
-  document.getElementById("qoute-box").innerHTML = quoteParagraph + sourceParagraph;
+  document.getElementById("quote-box").innerHTML = quoteParagraph + sourceParagraph;
 }
-printQuote();
+
 
 
 
